849a2beb006c25535a05dea58d7a61be8ee78450
lzacchi <zacchilucasm@gmail.com>
2021-03-29 22:09:13 -0300

    Add core instruction set, a few OpCodes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
-function toggleElement(id) {
-  var x = document.getElementById(id);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+function showElement_(id) {
+  var element = document.getElementById(id);
+  element.style.display = 'block';
+}
+
+function hideElement(id) {
+  var element = document.getElementById(id);
+  element.style.display = 'none';
+}
+
+function filterInstruction() {
+  var td, i, tdArr, j, alltables;
+  var input = document.getElementById("instruction-search");
+  var filter = input.value.toUpperCase();
+  // var table = document.getElementById("instruction-table");
+
+  alltables = document.querySelectorAll("table[data-name=instruction-table]");
+
+  alltables.forEach(function (table) {
+    // Loop through all table rows
+    var tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      tdArr = tr[i].getElementsByTagName("td");
+      // Loop through all table columns in the current row, and hide those who don't match the search query
+      firstCol = tdArr[0];
+      secondCol = tdArr[1]
+      if (firstCol || secondCol) {
+        if ((firstCol.innerHTML.toUpperCase().indexOf(filter) > -1) ||
+          (secondCol.innerHTML.toUpperCase().indexOf(filter) > -1)) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  });
 }
