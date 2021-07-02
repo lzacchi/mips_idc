065872105117daae2a40a0eeb4028b5b53026d30
Lucas Zacchi <zacchilucasm@gmail.com>
2021-07-02 11:45:34 -0300

    Fix PC+4 not being taken into account during branch range calculations

--- js/rangeCalculator.js
+++ js/rangeCalculator.js
@@ -6,9 +6,9 @@ function branchRange(input) {
     maxValid = "0x1FFFC";
     minValid = "0x20000"
 
-    var maxRange = (parseInt(input.slice(2), 16)  + parseInt(maxValid, 16)).toString(16);
+    var maxRange = (parseInt(input.slice(2), 16) + 4  + parseInt(maxValid, 16)).toString(16);
 
-    var minRange = (parseInt(input.slice(2), 16)  - parseInt(minValid, 16)).toString(16);
+    var minRange = (parseInt(input.slice(2), 16) + 4 - parseInt(minValid, 16)).toString(16);
 
     document.getElementById('branch-max').innerHTML = "The maximum reachable address by this branch instruction is 0x" + maxRange.padStart(8, '0');
     document.getElementById('branch-min').innerHTML = "The minimum reachable address by this branch instruction is 0x" + minRange.padStart(8, '0');
