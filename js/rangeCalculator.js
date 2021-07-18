4a5e43e0cfc224824299eadb5d0ca90651b604c8
Lucas Zacchi <zacchilucasm@gmail.com>
2021-07-18 18:01:56 -0300

    Fix jump instruction range calculation

--- js/rangeCalculator.js
+++ js/rangeCalculator.js
@@ -14,3 +14,12 @@ function branchRange(input) {
     document.getElementById('branch-min').innerHTML = "The minimum reachable address by this branch instruction is 0x" + minRange.padStart(8, '0');
 
 }
+
+function jumpRange(input) {
+    var maxRange = input.slice(2, 3) + "FFF FFFC";
+    var minRange = input.slice(2,3) + "000 0000";
+
+    document.getElementById('jump-max').innerHTML = "The maximum reachable address by this branch instruction is 0x" + maxRange.padStart(8, '0');
+    document.getElementById('jump-min').innerHTML = "The minimum reachable address by this branch instruction is 0x" + minRange.padStart(8, '0');
+
+}
