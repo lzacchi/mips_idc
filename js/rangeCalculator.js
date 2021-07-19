a77cab401117acc8448f231d938d067e52fe298d
Lucas Zacchi <zacchilucasm@gmail.com>
2021-07-19 09:43:18 -0300

    Add Branch and Jump explanations

--- js/rangeCalculator.js
+++ js/rangeCalculator.js
@@ -1,14 +1,17 @@
 /*  Range de beq(Somar PC+4 e depois:):
     Hex:    [ (-) 0x20000, (+) 0x1FFFC]*/
 
-
 function branchRange(input) {
     maxValid = "1FFFC";
     minValid = "20000"
 
-    var maxRange = (parseInt(input.slice(2), 16) + 4  + parseInt(maxValid, 16)).toString(16);
-
-    var minRange = parseInt(input.slice(2), 16) + 4 - parseInt(minValid, 16);
+    if (document.getElementById('pc_checkbox').checked) {
+        var maxRange = (parseInt(input.slice(2), 16) + parseInt(maxValid, 16)).toString(16);
+        var minRange = parseInt(input.slice(2), 16) - parseInt(minValid, 16);
+    } else {
+        var maxRange = (parseInt(input.slice(2), 16) + 4 + parseInt(maxValid, 16)).toString(16);
+        var minRange = parseInt(input.slice(2), 16) + 4 - parseInt(minValid, 16);
+    }
 
     if (minRange < 0) {
         minRange = 0;
@@ -23,7 +26,7 @@ function branchRange(input) {
 
 function jumpRange(input) {
     var maxRange = input.slice(2, 3) + "FFF FFFC";
-    var minRange = input.slice(2,3) + "000 0000";
+    var minRange = input.slice(2, 3) + "000 0000";
 
     document.getElementById('jump-max').innerHTML = "The maximum reachable address by this branch instruction is 0x" + maxRange.padStart(8, '0');
     document.getElementById('jump-min').innerHTML = "The minimum reachable address by this branch instruction is 0x" + minRange.padStart(8, '0');
